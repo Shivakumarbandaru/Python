cab974aa80ae5e527b89368bf28ed26b70504710
Shivakumarbandaru <78898866+Shivakumarbandaru@users.noreply.github.com>
2021-03-04 09:02:29 +0530

    Update textToSpeech.py

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,11 +1,3 @@
-"""from gtts import gTTS
-from playsound import playsound
-mytext = input("Enter text: ")
-file = "text.mp3"
-language = 'en'
-myobject = gTTS(text = mytext, lang = language, slow = False)
-myobject.save(file)
-playsound(file)"""
 import pyttsx3
 playAudio = pyttsx3.init()
 playAudio.say(input("Enter a text: "))

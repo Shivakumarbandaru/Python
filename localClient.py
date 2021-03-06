051471b7f807f12f65c6f628654d00f6b2e95fc6
Shivakumarbandaru <78898866+Shivakumarbandaru@users.noreply.github.com>
2021-03-06 13:27:18 +0530

    Add files via upload

---
+++ localClient.py
@@ -0,0 +1,10 @@
+''' Local client for Local server '''
+import socket               				# Import socket module
+
+socketObject = socket.socket()     		    # Create a socket object
+host = socket.gethostname()					# Get local machine name
+port = 12345                                # Reserve a port for your service.
+
+socketObject.connect((host, port)) 			# This method actively initiates TCP server connection.
+print (socketObject.recv(1024)) 		    # Receives message
+socketObject.close()              	        # Close the socket when done

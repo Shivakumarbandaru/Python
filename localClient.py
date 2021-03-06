5018fedadb0ef46a7a6463ffad8a960afd994970
Shivakumarbandaru <78898866+Shivakumarbandaru@users.noreply.github.com>
2021-03-06 13:34:55 +0530

    Update localClient.py

--- localClient.py
+++ localClient.py
@@ -6,5 +6,5 @@ host = socket.gethostname()					# Get local machine name
 port = 12345                                # Reserve a port for your service.
 
 socketObject.connect((host, port)) 			# This method actively initiates TCP server connection.
-print (socketObject.recv(1024)) 		    # Receives message
+print(socketObject.recv(1024)) 		    # Receives message
 socketObject.close()              	        # Close the socket when done

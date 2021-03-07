85e76b490c4526817d4500a14804c45b26e1e7d8
Shivakumarbandaru <78898866+Shivakumarbandaru@users.noreply.github.com>
2021-03-07 11:48:43 +0530

    Update clientForRemoteServer.py

--- clientForRemoteServer.py
+++ clientForRemoteServer.py
@@ -3,7 +3,7 @@ import socket               				# Import socket module
 
 socketObject = socket.socket()     		    # Create a socket object
 host = '165.22.14.77'
-port = 12346                               # Reserve a port for your service.
+port = 9999                             # Reserve a port for your service.
 
 socketObject.connect((host, port))		# This method actively initiates TCP server connection.
 print(socketObject.recv(1024)) 		    # Receives message
